81dd46a62caf5d061b121b092a76c617cb3b8e08
Johang727 <dev@johang.dev>
2026-04-27 21:08:53 -0500

    Changed api to be hosted on johang.dev

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.tecsrcalc.org";
+const API_URL = "https://tec-api.johang.dev";
 
 async function get_prediction() {
     const DPM = document.getElementById("dpm").value;
